ea5507e2808011b5ece8c0b0ff30cade246498cb
alex <zeimedee@gmail.com>
2020-01-23 23:42:35 +0000

    day4

---
+++ day4.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 10; i++) {
+
+    for (let j = 1; j <= 12; j++) {
+        console.log(`${i} x ${j} = ${i*j}`);
+        
+    }
+    console.log('\n');
+    
+}
---
+++ day_4.js
@@ -0,0 +1,10 @@
+/*
+*Author: Alexander Domkyaareh
+*/
+let total = 0;
+for (let i = 1; i <= 10; i++){
+console.log(`${total} + ${i} = ${total += i}`);       
+}
+console.log(`sum = ${total}`);
+
+
