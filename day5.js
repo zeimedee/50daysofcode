e3d1bab8c63faaf588568c4a725b154df2b93d63
alex <zeimedee@gmail.com>
2020-01-25 00:39:48 +0000

    day5

---
+++ day5.js
@@ -0,0 +1,13 @@
+/*
+*Author: Alexander Domakyaareh
+*File: calculate 10!
+*/
+
+var total = 1;
+
+for (let i = 1; i <= 10; i++) {
+   console.log(`${total} x ${i} = ${total *= i}`);
+      
+}
+
+console.log(`total = ${total}`);
