e7736f914b91f7294206917184d0fbb655f06f08
alex <zeimedee@gmail.com>
2020-01-28 16:11:59 +0000

    day9

---
+++ day9.js
@@ -0,0 +1,15 @@
+/*
+*Author Alexander Domakyaareh
+*File: calculate the sum of 10 numbers in an array
+*/
+
+let num = [1,2,3,4,5,6,7,8,9,10];
+let sum = 0;
+output = '1 + ';
+
+num.map((el)=>{
+        sum += el;  
+        
+});
+
+console.log(`total = ${sum}`);
