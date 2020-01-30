0521953fb42e761435227586400df1a13cd03b0c
alex <zeimedee@gmail.com>
2020-01-30 17:17:48 +0000

    day 11

---
+++ day11.js
@@ -0,0 +1,18 @@
+/*
+*Author: Alexander Domakyaareh
+*File: function the receives an array of number as an argument and returns an array of only the positive numbers
+*/
+
+let array =[1,-2,3,-9,-5,4,3,7,10];
+let result =[];
+
+const positiveArray =(args)=>{
+    array.map((el)=>{
+        if( el > 0){
+            result.push(el);
+        }
+    });
+    console.log(`positive numbers in [${array}] are [${result}]`);
+
+}
+positiveArray(array);
