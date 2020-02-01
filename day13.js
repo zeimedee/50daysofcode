e79b07c4beae49bc634559c22f153b623864fc0a
alex <zeimedee@gmail.com>
2020-02-01 22:35:44 +0000

    day13

---
+++ day13.js
@@ -0,0 +1,19 @@
+/*
+*Author: Alexander Domakyaareh
+*File: print first 10 fibonacci numbers without recursion
+*/
+
+let fib=[]
+let currentNumber=1;
+let previousNumber = 0
+let temp;
+
+for (let i = 1; i <= 10; i++) {
+temp = currentNumber;
+currentNumber = previousNumber + currentNumber;
+previousNumber = temp;
+fib.push(previousNumber)
+
+
+}
+console.log(`the first 10 fibonacci numbers are [${fib}]`);
