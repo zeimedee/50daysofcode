dd3274b8bd037fa3576dade1d7143a6867276d8d
alex <zeimedee@gmail.com>
2020-02-04 22:45:57 +0000

    day16

---
+++ day16.js
@@ -0,0 +1,14 @@
+/*
+*Author: Alexander Domakyaareh
+*File: function to reverse an array
+*/
+
+let array = ['alex', 'cat', 'zoro', 1];
+
+const reverseArray = (input) => {
+    return input.reverse();
+}
+
+
+console.log(`the reverse of [${array}] is {${reverseArray(array)}] `);
+
