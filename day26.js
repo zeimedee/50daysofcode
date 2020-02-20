574d203a6af9b7ff4659254351cc0a2798a34f2f
alex <zeimedee@gmail.com>
2020-02-20 23:03:10 +0000

    day27

---
+++ day26.js
@@ -0,0 +1,12 @@
+/*
+*Author: Alexander Domakyaareh
+*File: convert a string into an array of characters
+*/
+
+
+const toArray=(str)=>{
+    let result = str.split('');
+    return result;
+}
+
+console.log(toArray("alex"));
---
+++ day27.js
@@ -0,0 +1,15 @@
+/*
+*Author: Alexander Domakyaareh
+*File: create a function that will convert a string to an array of its ASCII codes of each character
+*/
+
+
+const toAscii = (str) => {
+
+    let result = str.split('').map(el=>{
+        return el +' = '+ el.charCodeAt(0); 
+    })
+  return result;
+}
+let text = 'alex123';
+console.log(toAscii(text));
