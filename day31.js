ef8f14f3d0f0a67a94edbc370ae7294286821525
alex <zeimedee@gmail.com>
2020-02-24 22:09:08 +0000

    day31

---
+++ day31.js
@@ -0,0 +1,11 @@
+/*
+*Author: Alexander Domakyaareh
+*File: write a function which will convert a binary number into a decimal number
+*/
+
+const biToDec = (input) => {
+    return parseInt(input, 2)
+}
+
+
+console.log(biToDec(10));
