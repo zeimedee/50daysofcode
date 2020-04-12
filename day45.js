ee15ae98dc34ad15c9168091f175f647edc57fa2
alex <zeimedee@gmail.com>
2020-04-12 23:26:41 +0000

    catch up

---
+++ day45.js
@@ -0,0 +1,23 @@
+
+
+const getMiddle = (word) => {
+    let len = word.length;
+    let index = len / 2 ;
+        if( //even
+            len % 2 == 0){
+                let mid = word[Math.floor(index)-1] + word[Math.floor(index)];
+                return mid;
+        }
+        if(//odd
+            len % 2 != 0){
+                    return word[Math.floor(index)];
+        }
+        if(len == 1){
+            return word;
+        }
+
+    
+}
+
+
+console.log(getMiddle('alexnaders'));
